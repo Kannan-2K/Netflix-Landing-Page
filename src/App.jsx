30ba4661d227b1db343a9a76e930463fff84d7ca
S.Kannan <78336020+Kannan-2K@users.noreply.github.com>
2025-03-21 00:03:51 +0530

    hero changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,8 @@ import Hero from "./component/Navbar/Hero";
 const App = () => {
   return (
     <div className="container">
-      <div className="opacity">
-        <Navbar />
-        <Hero />
-      </div>
+      <Navbar />
+      <Hero />
     </div>
   );
 };
--- src/component/Navbar/Hero.jsx
+++ src/component/Navbar/Hero.jsx
@@ -2,15 +2,29 @@ import React from "react";
 import "./Hero.css";
 const Hero = () => {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        flexGrow: 1,
+        // flex: 1,
+        minHeight: "100%",
+        textAlign: "center",
+        gap: "28px",
+      }}
+    >
       <h1 className="text">Unlimited movies,Tv show and more</h1>
       <h5 className="sub-heading">Watch anywhere.cancel anytime</h5>
-      <h5 className="sub-heading1">Ready to watch ? Enter your Email to create or restart your membership</h5>
-     
-      <div className="button1">
-      <input type="email" placeholder="Email address" className="email"/>
+      <h5 className="sub-heading1">
+        Ready to watch ? Enter your Email to create or restart your membership
+      </h5>
+      <div className="buttons">
+        <div className="button1">
+          <input placeholder="Email address" className="email" />
+        </div>
+        <button className="button2">Get Started &gt;</button>
       </div>
-      <button className="button2">Get Started &gt;</button>
     </div>
   );
 };
